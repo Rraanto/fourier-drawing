de0c6dda0c23ae29e277b14529e879d5faee2af6
Rraanto <ranto.rakotondrajoa@gmail.com>
2023-12-04 18:41:31 +0100

    Added and tested image module

--- main.cc
+++ main.cc
@@ -3,9 +3,15 @@
 #include <cmath>
 #include <GL/glut.h>
 #include <vector>
+#include <opencv2/opencv.hpp>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 #include "nombres_complexes.h"
 #include "utils.h"
+#include "image.h"
 
 using namespace std;
 using std::vector;
@@ -39,16 +45,13 @@ int main(int argc, char **argv) {
   return 0;
   */
 
-  complexe z = complexe(1, 1);
-  affiche(z / 2);
-  cout << "\n";
+  if (argc != 2) {
+    cout << "add exactly one (01) path to an image as argument\n";
+    return(1);
+  }
 
-  affiche (1 / z);
-  cout << "\n";
+  Mat img = imread(argv[1]);
+  vector<complexe> signal = generer_signal(contour_plus_long(img));
 
-  affiche(2 * z);
-  cout << "\n";
-
-  affiche(z * 2);
-  cout << "\n";
+  cout << "Signal de taille " << signal.size() << "\n";
 }
