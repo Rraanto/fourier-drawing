fbdac25a9687e31a456ef0953f20100bd0bfec45
rraanto <ranto.rakotondrajoa@gmail.com>
2026-02-07 23:19:06 +0100

    restructuring logical app handling away from the main executable

--- libs/Fourier/fourier.cpp
+++ libs/Fourier/fourier.cpp
@@ -1,23 +1,31 @@
-#include <vector>
+#include "fourier.h"
+#include <algorithm>
 #include <cmath>
 #include <complex>
-#include "fourier.h"
+#include <vector>
 
 using namespace std;
-using std::vector;
 using std::complex;
+using std::vector;
 
 vector<complex<double>> tdf(vector<complex<double>> S, int NB_COEFFS) {
+  /*
+   * calcule la transformée de Fourier à partir d'un signal complexe S
+   * S: un ensemble de nombres complexes (x, y)
+   */
   vector<complex<double>> res;
   int N = S.size();
-  int m = N/2;
-  int sup = NB_COEFFS/2;
+  int m = N / 2;
+  int sup = NB_COEFFS / 2;
+
   for (int k = -sup; k < sup; k++) {
     complex<double> z(0.0, 0.0); // somme
+
+    // intégration discrete
     for (int n = -m; n < m; n++) {
-      complex<double> x = S[n + m]; // facteur de l'exp dans le coeff 
-      double angle = 2*k*M_PI*n/N;
-      z = z + x * complex<double>(cos(angle) / N, -sin(angle) / N);
+      complex<double> x = S[n + m]; // facteur de l'exp dans le coeff
+      double angle = 2.0 * (double)k * M_PI * (double)n / (double)N;
+      z += x * complex<double>(cos(angle) / N, -sin(angle) / N);
     }
     res.push_back(z);
   }
